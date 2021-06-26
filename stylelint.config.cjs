96827f6f338d0c033d995eb412e642caedf20415
Stephen Wade <stephen@stephenwade.me>
2021-06-25 20:13:18 -0400

    Add Stylelint

---
+++ stylelint.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-hudochenkov/order',
+  ],
+  rules: {
+    'declaration-block-trailing-semicolon': 'always',
+  },
+};
