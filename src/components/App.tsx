defc2576f543c4d2f50a25698f5bc895d134cb58
Stephen Wade <stephen@stephenwade.me>
2021-06-30 18:25:59 -0400

    Add error toast

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 import Sidebar from './Sidebar';
 import ContactEditor from './ContactEditor';
@@ -10,12 +11,25 @@ function App(): JSX.Element {
   const error = useAppSelector((state) => state.error);
   const loading = useAppSelector((state) => state.loading);
 
+  if (error) {
+    toast.error(
+      <span className="AppToastText">
+        Something went wrong.
+        <button onClick={() => window.location.reload()}>Reload</button>
+      </span>,
+      {
+        duration: 5000,
+        position: 'bottom-left',
+        id: 'error',
+      }
+    );
+  }
+
   return (
     <div className="App">
+      <Toaster />
       <Sidebar />
       <ContactEditor loading={loading} />
-      {/* TODO: Add error snackbar */}
-      {error && <h2>Error</h2>}
     </div>
   );
 }
