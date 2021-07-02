ec6bb90f110149219579d66febee7303f67bbad2
Stephen Wade <stephen@stephenwade.me>
2021-07-02 17:13:53 -0400

    Accessibility: Use <button> instead of <span>

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -6,12 +6,12 @@ function AddButton(props: { onClick?: () => void }): JSX.Element {
   const { onClick } = props;
 
   return (
-    <span className="AddButton" onClick={onClick}>
+    <button className="AddButton" onClick={onClick}>
       <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
         <circle cx="16" cy="16" r="16" />
         <path strokeWidth="2" stroke="white" d="M 4 16h 24M 16,4v 24Z" />
       </svg>
-    </span>
+    </button>
   );
 }
 
--- src/components/RemoveButton.tsx
+++ src/components/RemoveButton.tsx
@@ -6,12 +6,12 @@ function RemoveButton(props: { onClick: () => void }): JSX.Element {
   const { onClick } = props;
 
   return (
-    <span className="RemoveButton" onClick={onClick}>
+    <button className="RemoveButton" onClick={onClick}>
       <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
         <circle cx="16" cy="16" r="16" />
         <path strokeWidth="2" stroke="white" d="M 4 16h 24Z" />
       </svg>
-    </span>
+    </button>
   );
 }
 
