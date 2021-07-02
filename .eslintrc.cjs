56ac95d1c542359af96506ae4c19e2d15a6e3e30
Stephen Wade <stephen@stephenwade.me>
2021-07-02 17:11:08 -0400

    Add ability to lint HTML files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
 
   parser: '@typescript-eslint/parser',
 
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'html'],
 
   extends: [
     'eslint:recommended',
