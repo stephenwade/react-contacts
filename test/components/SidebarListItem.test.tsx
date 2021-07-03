5557b33bd183e1ae7f07626bf11023d6502196b6
Stephen Wade <stephen@stephenwade.me>
2021-07-03 15:48:50 -0400

    Add tests for SidebarListItem

---
+++ test/components/SidebarListItem.test.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import SidebarListItem from '../../src/components/SidebarListItem';
+import { Contact } from '../../src/types';
+
+describe('existing contact', () => {
+  const testContact: Contact = {
+    id: 61,
+    firstName: 'Faye',
+    lastName: 'Armstrong',
+    emails: [],
+  };
+
+  test('Visible text', () => {
+    render(<SidebarListItem contact={testContact} active={false} />);
+
+    expect(screen.getByText('Faye Armstrong')).toBeTruthy();
+  });
+
+  test('active', () => {
+    render(<SidebarListItem contact={testContact} active={false} />);
+
+    expect(document.querySelector('.active')).toBeFalsy();
+
+    render(<SidebarListItem contact={testContact} active={true} />);
+
+    expect(document.querySelector('.active')).toBeTruthy();
+  });
+
+  test('onClick', () => {
+    const handleClick = jest.fn();
+
+    render(
+      <SidebarListItem
+        contact={testContact}
+        active={false}
+        onClick={handleClick}
+      />
+    );
+
+    screen.getByText('Faye Armstrong').click();
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  test('onKeyDown behaves the same as onClick', () => {
+    const handleClick = jest.fn();
+    const enterKeyDownEvent = new KeyboardEvent('keydown', {
+      bubbles: true,
+      key: 'Enter',
+    });
+
+    render(
+      <SidebarListItem
+        contact={testContact}
+        active={false}
+        onClick={handleClick}
+      />
+    );
+
+    screen.getByText('Faye Armstrong').dispatchEvent(enterKeyDownEvent);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('new contact', () => {
+  test('Visible text', () => {
+    render(<SidebarListItem new />);
+
+    expect(screen.getByText('New contact')).toBeTruthy();
+  });
+});
